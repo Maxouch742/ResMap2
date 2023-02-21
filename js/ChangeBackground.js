370b56de496fc20cfd87653fd73b462816d625f7
Maxime Fourquaux <maxime.fourquaux@heig-vd.ch>
2023-02-21 22:36:09 +0100

    add dropdown layers
    Il n'est pas cliquable

--- js/ChangeBackground.js
+++ js/ChangeBackground.js
@@ -40,3 +40,27 @@ function changeBackground() {
         map.removeLayer(MO_nb);
     };
 };
+
+
+/*************************** DROPDOWN pour les layers ***************************/
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function functionLayers(){
+    document.getElementById("myDropdown").classList.toggle("show");
+};
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
