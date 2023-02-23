5239f11c662c331c4f9b0e70b126b305a4e57a1f
Maxime Fourquaux <maxime.fourquaux@heig-vd.ch>
2023-02-23 16:44:26 +0100

    switch to remplace else if

--- js/ChangeBackground.js
+++ js/ChangeBackground.js
@@ -10,78 +10,20 @@ function changeBackground(layer) {
     map.removeLayer(SwissSURFACE3D);
     map.removeLayer(MO_nb);
 
-    if (layer === 'CN'){
-        map.addLayer(carteNationale);
-    } else if (layer === 'swissImage'){
-        map.addLayer(swissImage);
-    } else if (layer === 'swissSurface3D'){
-        map.addLayer(SwissSURFACE3D);
-    } else if (layer === 'MO'){
-        map.addLayer(MO_nb);
-    } ;
-
-
-    /*
-
-    if (document.getElementById("cb_CN").checked == false) {
-        map.removeLayer(carteNationale);
+    // Ajout du layer choisi par l'utilisateur
+    switch (layer){
+        case 'CN' :
+            map.addLayer(carteNationale);
+            break;
+        case 'swissImage' :
+            map.addLayer(swissImage);
+            break;
+        case 'swissSurface3D' :
+            map.addLayer(SwissSURFACE3D);
+            break;
+        case 'MO' :
+            map.addLayer(MO_nb);
+            break;
     };
-    if (document.getElementById("cb_CN").checked == true) {
-        map.addLayer(carteNationale);
-    };
-
-    if (document.getElementById("cb_MO").checked == false) {
-        map.removeLayer(MO_nb);
-    };
-    if (document.getElementById("cb_MO").checked == true) {
-        map.addLayer(MO_nb);
-    };
-
-    if (document.getElementById("cb_img").checked == false) {
-        map.removeLayer(swissImage);
-    };
-    if (document.getElementById("cb_img").checked == true) {
-        map.addLayer(swissImage);
-    };
-
-    if (document.getElementById("cb_relief").checked == false) {
-        map.removeLayer(SwissSURFACE3D);
-    };
-    if (document.getElementById("cb_relief").checked == true) {
-        map.addLayer(SwissSURFACE3D);
-    };
-
-
-    if (document.getElementById("cb_none").checked == true) {
-        map.removeLayer(swissImage);
-        map.removeLayer(carteNationale);
-        map.removeLayer(SwissSURFACE3D);
-        map.removeLayer(MO_nb);
-    };
-
-    */
 };
 
-
-/*************************** DROPDOWN pour les layers ***************************/
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function functionLayers(){
-    document.getElementById("myDropdown").classList.toggle("show");
-};
-
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
