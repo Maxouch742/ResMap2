2294e93e1ff2cac8c65cd353821efc072c4748ab
Maxime Fourquaux <maxime.fourquaux@heig-vd.ch>
2023-07-13 12:25:13 +0200

    fix bug to zi gnss

--- js/affichFiabLocPlani.js
+++ js/affichFiabLocPlani.js
@@ -3,7 +3,7 @@ function affichFiabLocPlani(xml, pts){
     // Create source
     const planiFiabLoc_source = new ol.source.Vector({});
     let planiGNSS_sessionID = 1;
-    const list_radius = [0.10, 0.13, 0.16, 0.19, 0.22, 0.25, 0.28, 0.31, 0.34];
+    const list_radius = [0.08, 0.12, 0.16, 0.20, 0.24, 0.28, 0.32, 0.36, 0.40];
     
     // Elements du fichier HTML
     const planiAbriss = xml.getElementsByTagName('planimetricAbriss')[0];
@@ -27,7 +27,6 @@ function affichFiabLocPlani(xml, pts){
                         const obs1 = obs[0];
                         if (obs1.getAttribute('obsNr') != ''){
                             const obs1_zi = parseFloat(obs1.getAttribute('zi'));
-                            console.log(obs1_zi);
                             const [colorFiab, widthFiab, zIndex] = getParameterFeature(obs1_zi);
                             
                             const planiFiabLoc_feature = new ol.Feature({ geometry: new ol.geom.Point([ pts.get(pt_name)['east'], pts.get(pt_name)['north'] ]) });
@@ -65,7 +64,6 @@ function affichFiabLocPlani(xml, pts){
         
         }
     }
-    console.log('ok');
 
     // Add to map
     planiFiabLoc_layer.setSource( planiFiabLoc_source );
