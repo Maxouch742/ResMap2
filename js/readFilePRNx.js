91491ff8d3dd4006ea44f34ba43bfced46faef60
Maxime Fourquaux <maxime.fourquaux@heig-vd.ch>
2023-07-25 21:33:57 +0200

    Check error LTOP

--- js/readFilePRNx.js
+++ js/readFilePRNx.js
@@ -11,46 +11,48 @@ function readFilePRNx(){
       xmlDoc = parser.parseFromString(xmlToParse,"text/xml");
 
       //TODO : traiter les erreurs (etat 14)
+      response = verifyErrors(xmlDoc);
+      if (response === 0){
 
-      // Points management
-      pts_Map = parseXML_points(xmlDoc);
-      const pts_planiVar = parseXML_variablePoints(xmlDoc, 'planimetric');
-      const pts_planiObs = parseXML_planiObsPoints(xmlDoc);  // get points to targets
-      const pts_altiVar = parseXML_variablePoints(xmlDoc, 'altimetric');
-      const pts_altiObs = parseXML_altiObsPoints(xmlDoc);
+        // Points management
+        pts_Map = parseXML_points(xmlDoc);
+        const pts_planiVar = parseXML_variablePoints(xmlDoc, 'planimetric');
+        const pts_planiObs = parseXML_planiObsPoints(xmlDoc);  // get points to targets
+        const pts_altiVar = parseXML_variablePoints(xmlDoc, 'altimetric');
+        const pts_altiObs = parseXML_altiObsPoints(xmlDoc);
 
-      defineViewByFile(pts_Map);   // Zoom view
-      affichPointsPlani(pts_Map, pts_planiVar, pts_planiObs);
-      affichPointsAlti(pts_Map, pts_altiVar, pts_altiObs);
+        defineViewByFile(pts_Map);   // Zoom view
+        affichPointsPlani(pts_Map, pts_planiVar, pts_planiObs);
+        affichPointsAlti(pts_Map, pts_altiVar, pts_altiObs);
 
-      // Measure
-      echelleEllipses = document.getElementById("myRange").value;
+        // Measure
+        echelleEllipses = document.getElementById("myRange").value;
       
+        //------- PLANIMETRIC
+        affichMeasPlani(xmlDoc, pts_Map);
+        affichPrecisionPlani(pts_Map, xmlDoc);
+        affichRectanglesPlani(pts_Map);
+        affichFiabLocPlani(xmlDoc, pts_Map);
+        affichResiNormesPlani(xmlDoc, pts_Map);
+        affichVecteurs(pts_Map);
 
-      //------- PLANIMETRIC
-      affichMeasPlani(xmlDoc, pts_Map);
-      affichPrecisionPlani(pts_Map, xmlDoc);
-      affichRectanglesPlani(pts_Map);
-      affichFiabLocPlani(xmlDoc, pts_Map);
-      affichResiNormesPlani(xmlDoc, pts_Map);
-      affichVecteurs(pts_Map);
+        //------- ALTIMETRIC
+        affichMeasAlti(xmlDoc, pts_Map);
+        affichPrecisionAlti(pts_Map, xmlDoc);
+        affichRectangleAlti(pts_Map, xmlDoc); 
+        affichFiabLocAlti(pts_Map, xmlDoc);   //TODO
+        affichResiNormesAlti(pts_Map, xmlDoc);//TODO
+        affichVecteursAlti(pts_Map);          //TODO
 
-      //------- ALTIMETRIC
-      affichMeasAlti(xmlDoc, pts_Map);
-      affichPrecisionAlti(pts_Map, xmlDoc);
-      affichRectangleAlti(pts_Map, xmlDoc); 
-      affichFiabLocAlti(pts_Map, xmlDoc);   //TODO
-      affichResiNormesAlti(pts_Map, xmlDoc);//TODO
-      affichVecteursAlti(pts_Map);          //TODO
+        //------ Relatif
+        affichPrecisionPlaniRela(pts_Map, xmlDoc); //TODO
+        affichRectanglePlaniRela(pts_Map, xmlDoc); //TODO
+        
+        affichPrecisionAltiRela(pts_Map, xmlDoc);  //TODO
+        affichRectangleAltiRela(pts_Map, xmlDoc);  //TODO
 
-      //------ Relatif
-      affichPrecisionPlaniRela(pts_Map, xmlDoc); //TODO
-      affichRectanglePlaniRela(pts_Map, xmlDoc); //TODO
-      
-      affichPrecisionAltiRela(pts_Map, xmlDoc);  //TODO
-      affichRectangleAltiRela(pts_Map, xmlDoc);  //TODO
-
-      document.getElementById("outputTest").textContent = "Import effectué ✓";
+        document.getElementById("outputTest").textContent = "Import effectué ✓";
+      };
     };
     fr.readAsText(fileName);
 
---
+++ js/verifyErrors.js
@@ -0,0 +1,21 @@
+function verifyErrors(xml){
+    
+    let response = 0;
+
+    // Niveau de confiance ellipses
+    const error = xml.getElementsByTagName("LTOPError");
+    if (error.length >= 1){
+        let error_string = "";
+        for (let i=0;i<error.length;++i){
+            const code = error[i].getAttribute("code");
+            error_string += "- "
+            error_string += code;
+            error_string += " ;\n";
+        }
+
+        alert("Le calcul n'a pas été exécuté correctement!\nLTOP retourne les erreurs suivantes :\n" + error_string);
+        response = 1;
+    }
+
+    return response;
+}
