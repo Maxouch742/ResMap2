4c62a90d0962f94cace368e26fbf9b31f59ff157
Maxime Fourquaux <maxime.fourquaux@heig-vd.ch>
2023-08-30 08:46:01 +0200

    modify parsing points

--- js/parseXML_points.js
+++ js/parseXML_points.js
@@ -45,18 +45,38 @@ function parseXML_points(xml){
         pt_name = pt.getAttribute('name');
         if (pt_name != 'NULLBERN'){
 
-            if (points.has(pt_name) === true){
+            let east_temp = parseFloat(pt.getAttribute('easting'));
+            if (east_temp > 450000.0 && east_temp < 2000000.0){
+                east_temp = east_temp + 2000000.0;
+            };
+            let north_temp = parseFloat(pt.getAttribute('northing'));
+            if (north_temp > 70000.0 && north_temp < 1000000.0){
+                north_temp = north_temp + 1000000.0;
+            };
 
-                if (pt.getAttribute('dy') != null){ points.get(pt_name).dE = parseFloat(pt.getAttribute('dy')) };
-                if (pt.getAttribute('dx') != null){ points.get(pt_name).dN = parseFloat(pt.getAttribute('dx')) };
-                if (pt.getAttribute('dh') != null){ points.get(pt_name).dH = parseFloat(pt.getAttribute('dh')) };
+            // Ajouter les données à la Map
+            const pt_object = {
+                type: pt.getAttribute('type'),
+                east: east_temp, //.toFixed(5),
+                north: north_temp, //.toFixed(5),
+                height: parseFloat(pt.getAttribute('height')), //.toFixed(5),
+                geoidUnd: parseFloat(pt.getAttribute('geoidUnd')), //.toFixed(5),
+                eta: parseFloat(pt.getAttribute('eta_cc')), //.toFixed(2),
+                xi: parseFloat(pt.getAttribute('xi_cc')), //.toFixed(2),
+            };
+            points.set(pt_name, pt_object)
 
-                if (pt.getAttribute('meanErrorA') != null){ points.get(pt_name).EMA = parseFloat(pt.getAttribute('meanErrorA')) };
-                if (pt.getAttribute('meanErrorB') != null){ points.get(pt_name).EMB = parseFloat(pt.getAttribute('meanErrorB')) };
-                if (pt.getAttribute('azimuthA') != null){ points.get(pt_name).Gis_EMA = parseFloat(pt.getAttribute('azimuthA')) };
+
+            // Vérifier la présence d'éléments de précision des points
+            if (pt.getAttribute('dy') != null){ points.get(pt_name).dE = parseFloat(pt.getAttribute('dy')) };
+            if (pt.getAttribute('dx') != null){ points.get(pt_name).dN = parseFloat(pt.getAttribute('dx')) };
+            if (pt.getAttribute('dh') != null){ points.get(pt_name).dH = parseFloat(pt.getAttribute('dh')) };
+
+            if (pt.getAttribute('meanErrorA') != null){ points.get(pt_name).EMA = parseFloat(pt.getAttribute('meanErrorA')) };
+            if (pt.getAttribute('meanErrorB') != null){ points.get(pt_name).EMB = parseFloat(pt.getAttribute('meanErrorB')) };
+            if (pt.getAttribute('azimuthA') != null){ points.get(pt_name).Gis_EMA = parseFloat(pt.getAttribute('azimuthA')) };
                 
-                if (pt.getAttribute('meanErrorH') != null){ points.get(pt_name).EMH = parseFloat(pt.getAttribute('meanErrorH')) };
-            }
+            if (pt.getAttribute('meanErrorH') != null){ points.get(pt_name).EMH = parseFloat(pt.getAttribute('meanErrorH')) };
         }
     };
 
