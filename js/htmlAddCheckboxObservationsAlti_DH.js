fe68847c3679fa13ea2c136118a30328db4fa967
maxime.fourquaux@heig-vd.ch <maxime.fourquaux@heig-vd.ch>
2023-09-29 13:57:13 +0200

    delete file inutilized

--- js/htmlAddCheckboxObservationsAlti_DH.js
+++
@@ -1,7 +0,0 @@
-function htmlAddCheckboxObservationsAlti_DH(){
-
-    const div = document.getElementById("obs_altitude_DH");
-    div.innerHTML = `<input class="form-check-input" type="checkbox" id="checkboxDH_alti" onchange="changeLayerVisibility('alti_DH')">`
-                  + `<label class="checkboxLabel" for="checkboxDH_alti" style="margin-left:5px">Obs. DH</label>`;
-    
-}
