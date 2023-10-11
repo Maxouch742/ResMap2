60decfb1e66ce07b74243676c85fa374f2e88420
Maxime Fourquaux <maxime.fourquaux@heig-vd.ch>
2023-10-11 09:12:50 +0200

    Finish. Utilisation: sidebar

---
+++ js/window_sidebar.js
@@ -0,0 +1,7 @@
+document.getElementById("openSidebar").addEventListener("click", function() {
+    document.getElementById("mySidebar").classList.add("open");
+});
+
+document.getElementById("closeSidebar").addEventListener("click", function() {
+    document.getElementById("mySidebar").classList.remove("open");
+});
