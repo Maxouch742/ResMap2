713eaa84ad46d9123297f187d89c3e0ff613e6f0
Maxime Fourquaux <maxime.fourquaux@heig-vd.ch>
2023-10-25 09:39:41 +0200

    zi and wi statistics
    TODO: check statistics

--- js/main.js
+++ js/main.js
@@ -7,21 +7,3 @@ setInteraction();
 
 matricule = false;
 matricule_sta = false;
-
-zi0_2D = 0;
-zi25_2D = 0;
-zi50_2D = 0;
-zi75_2D = 0;
-
-zi0_1D = 0;
-zi25_1D = 0;
-zi50_1D = 0;
-zi75_1D = 0;
-
-wiInf_2D = 0;
-wi2_2D = 0;
-wi0_2D = 0;
-
-wiInf_1D = 0;
-wi2_1D = 0;
-wi0_1D = 0;
--- js/readFilePRNx.js
+++ js/readFilePRNx.js
@@ -14,6 +14,24 @@ function readFilePRNx(){
       const parser = new DOMParser();
       xmlDoc = parser.parseFromString(xmlToParse,"text/xml");
 
+      zi0_2D = 0;
+      zi25_2D = 0;
+      zi50_2D = 0;
+      zi75_2D = 0;
+
+      zi0_1D = 0;
+      zi25_1D = 0;
+      zi50_1D = 0;
+      zi75_1D = 0;
+
+      wiInf_2D = 0;
+      wi2_2D = 0;
+      wi0_2D = 0;
+
+      wiInf_1D = 0;
+      wi2_1D = 0;
+      wi0_1D = 0;
+
       verifyErrors(xmlDoc);
       
       // Measure
